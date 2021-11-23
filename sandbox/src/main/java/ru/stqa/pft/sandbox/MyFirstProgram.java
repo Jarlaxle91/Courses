f91926cfcace3a2532733bf7c05f4aa9fc6332d2
Jarlaxle91 <Jarlaxle91@gmail.com>
2021-11-23 21:15:05 +1000

    Потренироваться использовать функции, классы, объекты и методы
    Ответы к заданию N2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,24 +7,29 @@ public class MyFirstProgram {
     hello("user");
     hello("Alexei");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p = new Point(3, 7, 1,11); //ToDo это задание 4
+    System.out.println("Расстояние между точками = " + p.distance());
+
+//    Point p1 = new Point(3, 7); //ToDo Это задание 3
+//    System.out.println("Точка P1 с координатами X = " + p1.x + " и Y = " + p1.y);
+//
+//    Point p2 = new Point(1, 11);
+//    System.out.println("Точка P2 с координатами X = " + p2.x + " и Y = " + p2.y);
+//    System.out.println("Расстояние между точками = " +  distance(p1, p2));
   }
+//  public static double distance(Point p1, Point p2) { //ToDo Это задание 3
+//  double d = Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+//  return d;
+//  }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
-
+  
 }
