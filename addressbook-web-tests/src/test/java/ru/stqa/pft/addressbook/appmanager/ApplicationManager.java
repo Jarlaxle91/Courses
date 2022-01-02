1c01fba9f66f53123978259b4c5f517f4c056b17
Jarlaxle91 <Jarlaxle91@gmail.com>
2022-01-02 18:44:36 +1000

    Добавлена возможность запускать тесты в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,19 +3,34 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 
- public class ApplicationManager {
-   WebDriver wd;
+public class ApplicationManager {
+  WebDriver wd;
 
-   private SessionHelper sessionHelper;
-   private NavigationHelper navigationHelper;
-   private GroupHelper groupHelper;
-   private ContactHelper contactHelper;
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  private ContactHelper contactHelper;
+  private String browser;
 
-   public void init() {
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
+  public void init() {
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
     System.setProperty("webdriver.chrome.driver", "c:/windows/system32/chromedriver.exe");
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -31,16 +46,20 @@ import java.util.concurrent.TimeUnit;
     wd.findElement(By.linkText("Logout")).click();
   }
 
-   public void stop() {
+  public void stop() {
     wd.quit();
   }
 
-   public GroupHelper getGroupHelper() {
+  public GroupHelper getGroupHelper() {
     return groupHelper;
   }
 
-   public NavigationHelper getNavigationHelper() { return navigationHelper; }
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
 
-   public ContactHelper getContactHelper() { return contactHelper; }
+  public ContactHelper getContactHelper() {
+    return contactHelper;
+  }
 
- }
+}
