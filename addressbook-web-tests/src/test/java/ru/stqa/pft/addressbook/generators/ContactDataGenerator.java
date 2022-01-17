d99f04daee62076c8fed936c5348fc893ae01b37
Jarlaxle91 <Jarlaxle91@gmail.com>
2022-01-17 23:21:10 +1000

    Реализованы генераторы тестовых данных, параметризованы тесты для создания групп и контактов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,68 @@
+package ru.stqa.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+  @Parameter(names = "-c", description = "Contacts count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = genetateContacts(count);
+    if (format.equals("json")) {
+      saveAsJson(contacts, new File(file));
+    } else {
+      System.out.println("Your format " + format + " does not match with JSON");
+    }
+  }
+
+  private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
+
+  private List<ContactData> genetateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData().withFirstName(String.format("first_name %s", i))
+              .withMiddleName(String.format("middle_name %s", i)).withLastName(String.format("last_name %s", i))
+              .withNickname(String.format("nickname %s", i)).withTitle(String.format("test_title %s", i))
+              .withCompany(String.format("test_company %s", i)).withAddress(String.format("test_address %s", i))
+              .withHomePhone("test_home").withMobilePhone("123456789").withWorkPhone("123456789").withFax("123456789")
+              .withFirstEmail("test1@test.com").withSecondEmail("test2@test.com").withThirdEmail("test3@test.com")
+              .withHomepage("www.homepage.com").withDayOfBirth("1").withMonthOfBirth("January").withYearOfBirth("1990")
+              .withAnniversaryDay("2").withAnniversaryMonth("February").withAnniversaryYear("2001").withGroup("test1"));
+    }
+    return contacts;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressBookEntry.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressBookEntry.java
@@ -1,32 +1,49 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AddressBookEntry extends TestBase {
 
-  @Test
-  public void testAddressBookContact() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testAddressBookContact(ContactData contact) throws Exception {
     app.goTo().contactPage();
     Contacts before = app.contact().all();
-    ContactData contact = new ContactData().withFirstName("Test first name").withMiddleName("Test middle name")
-            .withLastName("Test last name").withNickname("Test nickname").withTitle("Test title")
-            .withCompany("Test company").withAddress("Test address").withHomePhone("Test home").withMobilePhone("1234567890")
-            .withWorkPhone("Test work").withFax("Test fax").withFirstEmail("test1@test.com")
-            .withSecondEmail("test2@test.com").withThirdEmail("test3@test.com").withHomepage("www.homepage.com")
-            .withDayOfBirth("1").withMonthOfBirth("January").withYearOfBirth("1990").withAnniversaryDay("2")
-            .withAnniversaryMonth("February").withAnniversaryYear("2001").withGroup("test1");
-
-    app.contact().addNewContact();
     app.contact().create(contact);
-    Contacts after = app.contact().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
 
+    assertThat(app.contact().getContactCount(), equalTo(before.size() + 1));
+    Contacts after = app.contact().all();
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 }
+
